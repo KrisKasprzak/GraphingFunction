35e8791c692461b2d9c767a1a89c379d0c3e9217
Kris Kasprzak <kris.kasprzak@yahoo.com>
2024-04-19 10:11:36 -0500

    Update Graph.ino

--- Graph.ino
+++ Graph.ino
@@ -52,61 +52,60 @@ Pin settings
 */
 
 #include <SPI.h>
-#include <SD.h>
 #include "Adafruit_HX8357.h"
 
 
 // These are 'flexible' lines that can be changed
 
-#define TFT_CS 53
-#define TFT_DC 48
-#define TFT_RST 8 // RST can be set to -1 if you tie it to Arduino's reset
-#define SD_CCS 42
+// for a nano
+#define TFT_CS 10
+#define TFT_DC 9
 
 
-#define LTBLUE    0xB6DF
-#define LTTEAL    0xBF5F
-#define LTGREEN   0xBFF7
-#define LTCYAN    0xC7FF
-#define LTRED     0xFD34
+#define LTBLUE 0xB6DF
+#define LTTEAL 0xBF5F
+#define LTGREEN 0xBFF7
+#define LTCYAN 0xC7FF
+#define LTRED 0xFD34
 #define LTMAGENTA 0xFD5F
-#define LTYELLOW  0xFFF8
-#define LTORANGE  0xFE73
-#define LTPINK    0xFDDF
-#define LTPURPLE  0xCCFF
-#define LTGREY    0xE71C
+#define LTYELLOW 0xFFF8
+#define LTORANGE 0xFE73
+#define LTPINK 0xFDDF
+#define LTPURPLE 0xCCFF
+#define LTGREY 0xE71C
 
-#define BLUE      0x001F
-#define TEAL      0x0438
-#define GREEN     0x07E0
-#define CYAN      0x07FF
-#define RED       0xF800
-#define MAGENTA   0xF81F
-#define YELLOW    0xFFE0
-#define ORANGE    0xFC00
-#define PINK      0xF81F
-#define PURPLE    0x8010
-#define GREY      0xC618
-#define WHITE     0xFFFF
-#define BLACK     0x0000
+#define BLUE 0x001F
+#define TEAL 0x0438
+#define GREEN 0x07E0
+#define CYAN 0x07FF
+#define RED 0xF800
+#define MAGENTA 0xF81F
+#define YELLOW 0xFFE0
+#define ORANGE 0xFC00
+#define PINK 0xF81F
+#define PURPLE 0x8010
+#define GREY 0xC618
+#define WHITE 0xFFFF
+#define BLACK 0x0000
 
-#define DKBLUE    0x000D
-#define DKTEAL    0x020C
-#define DKGREEN   0x03E0
-#define DKCYAN    0x03EF
-#define DKRED     0x6000
+#define DKBLUE 0x000D
+#define DKTEAL 0x020C
+#define DKGREEN 0x03E0
+#define DKCYAN 0x03EF
+#define DKRED 0x6000
 #define DKMAGENTA 0x8008
-#define DKYELLOW  0x8400
-#define DKORANGE  0x8200
-#define DKPINK    0x9009
-#define DKPURPLE  0x4010
-#define DKGREY    0x4A49
+#define DKYELLOW 0x8400
+#define DKORANGE 0x8200
+#define DKPINK 0x9009
+#define DKPURPLE 0x4010
+#define DKGREY 0x4A49
 
 
 #define ADJ_PIN A0
 
 double a1, b1, c1, d1, r2, r1, vo, tempC, tempF, tempK;
-
+double x, y;
+double xLow = 0, xHigh = 60;
 Adafruit_HX8357 tft = Adafruit_HX8357(TFT_CS, TFT_DC);
 
 // this is the only external variable used by the graph
@@ -120,7 +119,7 @@ boolean display6 = true;
 boolean display7 = true;
 boolean display8 = true;
 boolean display9 = true;
-double ox , oy ;
+double ox, oy;
 
 void setup() {
 
@@ -130,124 +129,99 @@ void setup() {
   tft.begin(HX8357D);
   tft.fillScreen(BLACK);
 
-  tft.setRotation(2);
-  a1 = 3.354016E-03 ;
-  b1 = 2.569850E-04 ;
-  c1 = 2.620131E-06 ;
-  d1 = 6.383091E-08 ;
-
-
-  double x, y;
-
-
   tft.setRotation(3);
-
 
   for (x = 0; x <= 6.3; x += .1) {
 
     y = sin(x);
-    Graph(tft, x, y, 60, 290, 390, 260, 0, 6.5, 1, -1, 1, .25, "Sin Function", "x", "sin(x)", DKBLUE, RED, YELLOW, WHITE, BLACK, display1);
-
+    Graph(tft, x, y, 60, 290, 390, 260, 0, 6.5, 1, -1, 1, .25, "Sin Function - Large Graph", "x", "sin(x)", DKBLUE, RED, YELLOW, WHITE, BLACK, display1);
   }
 
-  delay(1000);
+  delay(5000);
 
   tft.fillScreen(BLACK);
   for (x = 0; x <= 6.3; x += .1) {
 
     y = sin(x);
-    Graph(tft, x, y, 100, 280, 100, 240, 0, 6.5, 3.25, -1, 1, .25, "Sin Function", "x", "sin(x)", GREY, GREEN, RED, YELLOW, BLACK, display9);
-
+    Graph(tft, x, y, 100, 280, 100, 240, 0, 6.5, 3.25, -1, 1, .25, "Sin Function - Narrow Graph", "x", "sin(x)", GREY, GREEN, RED, YELLOW, BLACK, display9);
   }
 
-    delay(1000);
+  delay(5000);
 
   tft.fillScreen(BLACK);
   for (x = 0; x <= 25.2; x += .1) {
 
     y = sin(x);
-    Graph(tft, x, y, 50, 190, 400, 60, 0, 25, 5, -1, 1, .5, "Sin Function", "x", "sin(x)", DKYELLOW, YELLOW, GREEN, WHITE, BLACK, display8);
-
+    Graph(tft, x, y, 50, 190, 400, 60, 0, 25, 5, -1, 1, .5, "Sin Function - Short Graph", "x", "sin(x)", DKYELLOW, YELLOW, GREEN, WHITE, BLACK, display8);
   }
 
-  delay(1000);
+  delay(5000);
 
   tft.fillScreen(BLACK);
   for (x = 0.001; x <= 10; x += .1) {
 
     y = log(x);
-    Graph(tft, x, y, 50, 240, 300, 180, 0, 10, 1, -10, 5, 1, "Natural Log Function", "x", "ln(x)", BLUE, RED, WHITE, WHITE, BLACK, display2);
-
+    Graph(tft, x, y, 50, 240, 300, 180, 0, 10, 1, -10, 5, 1, "Natural Log Function - Small Graph", "x", "ln(x)", BLUE, RED, WHITE, WHITE, BLACK, display2);
   }
 
-  
-
-  delay(1000);
+  delay(5000);
   tft.fillScreen(BLACK);
 
   for (x = 0; x <= 10; x += 1) {
 
     y = x * x;
     Graph(tft, x, y, 50, 290, 390, 260, 0, 10, 1, 0, 100, 10, "Square Function", "x", "x^2", DKRED, RED, YELLOW, WHITE, BLACK, display3);
-
   }
 
-  delay(1000);
+  delay(5000);
   tft.fillScreen(BLACK);
 
   for (x = 0.00; x <= 20; x += .01) {
 
     y = ((sin(x)) * x + cos(x)) - log(x);
     Graph(tft, x, y, 50, 290, 390, 260, 0, 20, 1, -20, 20, 5, "Weird Function", "x", " y = sin(x) + cos(x) - log(x)", ORANGE, YELLOW, CYAN, WHITE, BLACK, display4);
-
   }
 
-  delay(1000);
+  delay(5000);
   tft.fillScreen(BLACK);
+
   tft.setRotation(2);
   for (x = 0; x <= 12.6; x += .1) {
 
     y = sin(x);
     Graph(tft, x, y, 50, 250, 150, 150, 0, 13, 3.5, -1, 1, 1, "Sin(x)", "x", "sin(x)", DKBLUE, RED, YELLOW, WHITE, BLACK, display5);
-
   }
+
+  while (1) {}
   tft.setRotation(3);
-  delay(1000);
+  delay(5000);
   tft.fillScreen(WHITE);
 
   for (x = 0; x <= 6.3; x += .05) {
 
     y = cos(x);
     Graph(tft, x, y, 100, 250, 300, 200, 0, 6.5, 3.25, -1, 1, 1, "Cos Function", "x", "cos(x)", DKGREY, GREEN, BLUE, BLACK, WHITE, display6);
-
   }
 
-  delay(1000);
+  delay(5000);
   tft.fillScreen(BLACK);
-
-
-  for (x = 0; x <= 60; x += 1) {
-    vo = analogRead(ADJ_PIN) / 204.6;
-    r1 = 9940;
-    r2 = ( vo * r1) / (5 - vo);
-
-    //equation from data sheet
-    tempK = 1.0 / (a1 + (b1 * (log(r2 / 10000.0))) + (c1 * pow(log(r2 / 10000.0), 2)) + (d1 * pow(log(r2 / 10000.0), 3)));
-    tempC  = ((tempK - 273.15) );
-    y = tempF  = (tempC * 1.8000) + 32.00;
-
-    Graph(tft, x, y, 50, 290, 390, 260, 0, 60, 10, 70, 90, 5, "Room Temperature", " Time [s]", "Temperature [deg F]", DKBLUE, RED, GREEN, WHITE, BLACK, display7);
-    delay(250);
-  }
-
- delay(1000);
-  tft.fillScreen(BLACK);
-
 }
 
 
 void loop(void) {
 
+  x++;
+  y = analogRead(ADJ_PIN) / 204.6;
+  Graph(tft, x, y, 50, 290, 390, 260, xLow, xHigh, 10, 0, 5, 1, "A0 Input", " Time [s]", "Volts", DKBLUE, RED, GREEN, WHITE, BLACK, display7);
+  delay(250);
+
+  if (x >= xHigh) {
+    //x = 0;
+    xLow += 60;
+    xHigh += 60;
+    display7 = true;
+    tft.fillScreen(BLACK);
+  }
 }
 
 /*
@@ -296,23 +270,22 @@ void Graph(Adafruit_HX8357 &d, double x, double y, double gx, double gy, double 
   if (redraw == true) {
 
     redraw = false;
-    ox = (x - xlo) * ( w) / (xhi - xlo) + gx;
+    ox = (x - xlo) * (w) / (xhi - xlo) + gx;
     oy = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
     // draw y scale
-    for ( i = ylo; i <= yhi; i += yinc) {
+    for (i = ylo; i <= yhi; i += yinc) {
       // compute the transform
-      temp =  (i - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+      temp = (i - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
 
-      if (i == 0) {
+      if (i == ylo) {
         d.drawLine(gx, temp, gx + w, temp, acolor);
-      }
-      else {
+      } else {
         d.drawLine(gx, temp, gx + w, temp, gcolor);
       }
 
       d.setTextSize(1);
       d.setTextColor(tcolor, bcolor);
-      d.setCursor(gx - 40, temp);
+      d.setCursor(gx - 30, temp);
       // precision is default Arduino--this could really use some format control
       d.println(i);
     }
@@ -321,11 +294,10 @@ void Graph(Adafruit_HX8357 &d, double x, double y, double gx, double gy, double 
 
       // compute the transform
 
-      temp =  (i - xlo) * ( w) / (xhi - xlo) + gx;
-      if (i == 0) {
+      temp = (i - xlo) * (w) / (xhi - xlo) + gx;
+      if (i == xlo) {
         d.drawLine(temp, gy, temp, gy - h, acolor);
-      }
-      else {
+      } else {
         d.drawLine(temp, gy, temp, gy - h, gcolor);
       }
 
@@ -339,40 +311,36 @@ void Graph(Adafruit_HX8357 &d, double x, double y, double gx, double gy, double 
     //now draw the labels
     d.setTextSize(2);
     d.setTextColor(tcolor, bcolor);
-    d.setCursor(gx , gy - h - 30);
+    d.setCursor(gx, gy - h - 30);
     d.println(title);
 
     d.setTextSize(1);
     d.setTextColor(acolor, bcolor);
-    d.setCursor(gx , gy + 20);
+    d.setCursor(gx, gy + 20);
     d.println(xlabel);
 
+    /*
+ // some day auto rotate
     d.setTextSize(1);
+  
     d.setTextColor(acolor, bcolor);
-    d.setCursor(gx - 30, gy - h - 10);
+    d.setCursor(gy - h - 10, gx-40);
     d.println(ylabel);
-
-
+*/
   }
 
   //graph drawn now plot the data
   // the entire plotting code are these few lines...
   // recall that ox and oy are initialized as static above
-  x =  (x - xlo) * ( w) / (xhi - xlo) + gx;
-  y =  (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
+  x = (x - xlo) * (w) / (xhi - xlo) + gx;
+  y = (y - ylo) * (gy - h - gy) / (yhi - ylo) + gy;
   d.drawLine(ox, oy, x, y, pcolor);
   d.drawLine(ox, oy + 1, x, y + 1, pcolor);
   d.drawLine(ox, oy - 1, x, y - 1, pcolor);
   ox = x;
   oy = y;
-
 }
 
 /*
   End of graphing functioin
 */
-
-
-
-
-
